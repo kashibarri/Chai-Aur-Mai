87e4862bf8b2e02e0328e781fced416783725f7c
Kashif Ali <38554893+kashibarri@users.noreply.github.com>
2024-01-16 10:35:05 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+
+//Data Types
+
+let favNmber = 10   //Number + (big int is another datatype)
+
+const myName = 'Kashif Ali'     //String 
+
+let isRaining = false   //Boolean
+
+let location;       //Undefined
+
+let myNumber = null     //Null (standalone value)
+
+//Object
+
+let aboutMe = {                     
+    MyName: 'Kashif Ali',
+    age: 28
+}                           
+
+//Array
+
+let favCars = ['Toyota Grande','Range Rover Sport',  'Toyota Fortuner', 'Toyota Revo']
