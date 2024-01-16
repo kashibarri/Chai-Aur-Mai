ae64cffd933eb0c122bc4764742fa8b07c1cb36f
Kashif Ali <38554893+kashibarri@users.noreply.github.com>
2024-01-16 15:13:33 +0500

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const favNumber = 10; //ES6 variable types
+let rndmEmail = "abc@gmail.com"; //ES6 variable types
+
+var oldVar = "purana variable define krnay ka tareqa";
+
+accountCity = "gb"; //undefined variable type
+
+let city; //undefined variable
